360e1b9ea51caa41b061a09a68d2836e43f7a4f9
avtaar <ammy9867@gmail.com>
2021-11-17 09:56:53 +0530

    init commit

---
+++ Test/Controllers/StudentController.cs
@@ -0,0 +1,75 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null)
+                return View();
+            else
+            {
+                var StudentInDb = _context.Students.FirstOrDefault(e => e.Id == id);
+                return View(StudentInDb);      
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Student student)
+        {
+            if (student.Id== 0)
+                _context.Students.Add(student);
+            else
+            {
+               _context.Entry<Student>(student).State = EntityState.Modified;
+               
+            }
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var StudentInDb = _context.Students.Find(id);
+            return View(StudentInDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete_Data(int id)
+        {
+            var StudentInDb = _context.Students.Find(id);
+            _context.Students.Remove(StudentInDb);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Filter(string str)
+        {
+           var result= _context.Students.Where(x => x.FirstName.Contains(str) || x.ClassName.Contains(str)|| x.LastName.Contains(str));
+
+            return View("Index", result);
+        }
+        
+
+    }
+}
---
+++ Test/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Test.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Class Name")]
+        public string ClassName { get; set; }
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Value should be greater than or equal to 1")]
+        [Display(Name ="Roll No.")]
+        public int RollNo { get; set; }
+        [Required]
+        [Range(0, 100,ErrorMessage="Percentage should be between 0 to 100")]
+        public decimal Percentage { get; set; }
+    }
+}
